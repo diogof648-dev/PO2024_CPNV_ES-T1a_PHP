f7a67f67fdb4137ddeb07557dbda09b197bf1c6b
Arthur-Bottemanne <arthurbottemanne@gmail.com>
2024-11-15 11:14:48 +0100

    fix: remove quotes

--- start.bat
+++ start.bat
@@ -1,6 +1,6 @@
 @echo off
-set DB_DSN="mysql:host=127.0.0.1;port=3306;dbname=YOUR_DB_NAME"
-set DB_USER="YOUR_USER"
-set DB_PASSWORD="YOUR_PASSWORD"
+set DB_DSN=mysql:host=127.0.0.1;port=3306;dbname=YOUR_DB_NAME
+set DB_USER=YOUR_USER
+set DB_PASSWORD=YOUR_PASSWORD
 php -S localhost:8080 -t public/
 pause
