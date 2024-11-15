9fcc27060975e52bc3f97a3cd696afd17a010996
Diogo da Silva Fernandes <diogosilvafernandes85@gmail.com>
2024-11-15 10:52:02 +0100

    feat: Add User model

---
+++ app/Models/User.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+class User extends Model
+{
+    public string $id;
+    public string $firstname;
+    public string $lastname;
+    public string $email;
+    public string $city;
+    public string $canton;
+    public string $zip;
+
+    public function __construct()
+    {
+        $this->columns = ["firstname", "lastname", "email", "city", "canton", "zip"];
+
+        parent::__construct();
+    }
+}
